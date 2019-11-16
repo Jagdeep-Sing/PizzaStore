25878d97bc5eab71abe4c76192afc1329ab8988c
Jagdeep.singh <jagdeep.singh@manipalglobal.com>
2019-11-16 20:12:43 +0530

    🍟Add to cart & UI Modifications🍕

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,7 +13,61 @@ export class HomeComponent implements OnInit {
   isLoading = false;
   visible = false;
   placement = 'left';
+  tableLoader = false;
+  tableSize = "small";
 
+  vegPdata: any = [
+    {
+      "title": "Deluxe Veggie",
+      "desc": "Black olives, capsicum, onion, grilled mushroom, corn, tomato, jalapeno & extra cheese 1",
+      "price": "270",
+      "image": "https://images.dominos.co.in/new_deluxe_veggie.jpg",
+      "_id": "981"
+    },
+    {
+      "title": "Indi Tandoori Paneer",
+      "desc": "Fresh onions, capsicum, olives, grilled mushroom, corn, tomato, jalapeno & extra cheese 2",
+      "price": "300",
+      "image": "https://images.dominos.co.in/IndianTandooriPaneer.jpg",
+      "_id": "982"
+    },
+    {
+      "title": "Veg Extravaganza",
+      "desc": "Black olives, capsicum, onion, grilled mushroom, corn, tomato, jalapeno & extra cheese",
+      "price": "250",
+      "image": "https://images.dominos.co.in/new_veg_extravaganza.jpg",
+      "_id": "983"
+    }
+  ];
+
+  nonvegPdata: any = [
+    {
+      "title": "Chicken Pepperoni",
+      "desc": "The wholesome flavour of tandoori masala with Chicken tikka, onion, red paprika & mint mayo",
+      "price": "500",
+      "image": "https://images.dominos.co.in/cheesepepperoni.png",
+      "_id": "090"
+    },
+    {
+      "title": "Indi Chicken Tikka",
+      "desc": "The wholesome flavour of tandoori masala with Chicken tikka, onion, red paprika & mint mayo",
+      "price": "435",
+      "image": "https://images.dominos.co.in/IndianTandooriChickenTikka.jpg",
+      "_id": "091"
+    },
+    {
+      "title": "Non Veg Supreme",
+      "desc": "The wholesome flavour of tandoori masala with Chicken tikka, onion, red paprika & mint mayo",
+      "price": "300",
+      "image": "https://images.dominos.co.in/new_non_veg_supreme.jpg",
+      "_id": "092"
+    }
+  ];
+  globalPrice = 0;
+
+  cartObj: any = [];
+  tempIndex = 0;
+  addedItemKeys: string[];
   constructor(private quoteService: QuoteService) {}
 
   ngOnInit() {
@@ -30,8 +84,24 @@ export class HomeComponent implements OnInit {
       });
   }
 
-  open(): void {
+  open(price: any, title: any, id: any): void {
     this.visible = true;
+  }
+  addtoCart(price: any, title: any, id: any): void{
+   // this.tableLoader = true;
+    this.globalPrice += +price;
+    if(this.cartObj[id] !== undefined) {
+      this.cartObj[id].qty++;
+      let tempPrice = parseInt(this.cartObj[id].price) + +price;
+      this.cartObj[id].price = tempPrice;
+    } else{
+      this.cartObj[id] = {"price": price,"title": title,"_id": id,"qty": 1, "index":this.tempIndex};
+      this.tempIndex++;
+    }
+
+    this.addedItemKeys = Object.keys(this.cartObj);
+    console.log(this.addedItemKeys)
+
   }
 
   close(): void {
